be0c639fc30ed5f491bc77ee5773ddbd3ec5c581
AMJADH195 <43226290+AMJADH195@users.noreply.github.com>
2019-02-12 10:36:54 +0530

    Add files via upload

---
+++ userinput.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class userinput
+{
+	public static void main(String[] args)
+	{
+          Scanner input=new Scanner(System.in);
+	  int a=input.nextInt();
+	  System.out.println("THE ENTERED INTEGER IS "+a);
+
+	}
+
+}
